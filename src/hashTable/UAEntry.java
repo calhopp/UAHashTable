3f085b0c4454b26606e4c521862535cf49adecaa
calhopp <calhopp@gmail.com>
2026-03-31 19:53:30 -0500

    starting work

--- src/hashTable/UAEntry.java
+++ src/hashTable/UAEntry.java
@@ -5,4 +5,15 @@ public class UAEntry {
 	int termID;
 	int docCount = -1;
 	int start = -1;
+	
+	public UAEntry(String word, int termID) {
+		this.word = word;
+		this.termID = termID;
+		this.docCount = -1;
+		this.start = -1;
+	}
+	
+	public String toString() {
+		return String.format("%-40s %-10d %-10d %-10d %n", word, termID, docCount, start);
+	}
 }
--- src/hashTable/UAHashTable.java
+++ src/hashTable/UAHashTable.java
@@ -1,5 +1,27 @@
 package hashTable;
 
 public class UAHashTable {
-
+	UAEntry[] entries;
+	int count;
+	int termID;
+	
+	public UAHashTable(int length) {
+		this.entries = new UAEntry[length];
+		this.count = 0;
+		this.termID = 0;
+	}
+	
+	public void set(String word) {
+		for(int k = 0; k < entries.length; k++) {
+			if(entries[(word.hashCode() + k) % entries.length] == null) {
+				entries[(word.hashCode() + k) % entries.length] = new UAEntry(word, termID);
+			} else if(entries[(word.hashCode() + k) % entries.length] == null) {
+				
+			}
+		}
+	}
+	
+	public void get(String word) {
+		
+	}
 }
