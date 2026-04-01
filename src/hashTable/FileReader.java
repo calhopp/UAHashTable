93b906b5c8d768b6cbfe1843a0963cb8b6f3f2b0
calhopp <calhopp@gmail.com>
2026-03-31 19:36:44 -0500

    Initial commit

---
+++ src/hashTable/FileReader.java
@@ -0,0 +1,14 @@
+package hashTable;
+
+public class FileReader {
+	UAHashTable hashTable;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void readFile() {
+		
+	}
+}
---
+++ src/hashTable/UAEntry.java
@@ -0,0 +1,8 @@
+package hashTable;
+
+public class UAEntry {
+	String word;
+	int termID;
+	int docCount = -1;
+	int start = -1;
+}
